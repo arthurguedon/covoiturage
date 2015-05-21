9f0a6610c05ab8acdca975377c6a57dc2a75d2c7
arthurguedon <arthur.guedon@utt.fr>
2015-05-21 16:49:37 +0200

    Create connnection_bd.php

---
+++ connnection_bd.php
@@ -0,0 +1,10 @@
+<?php
+define ('HOTE' , 'localhost');
+define ('PSEUDO' , 'root');
+define ('MDP' , '');
+define ('NOM_BASE' , 'bd_covoiturage');
+
+$database = mysqli_connect (HOTE ,PSEUDO,MDP,NOM_BASE); 
+
+
+?>
